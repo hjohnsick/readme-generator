f20510a4aeeab489dd64085058531c369cf9f3c6
hjohnsick <hjohnsick@yahoo.com>
2022-01-29 09:25:52 -0500

    removed readme and added array of questions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project title?", "Enter a description of your project?", "Enter installation instructions", "Enter usage information",
+"Enter contribution guidelines", "Enter test instructions", "Choose a license", "Enter your GitHub username", "Enter your email address"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
