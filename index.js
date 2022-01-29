110a327a4b4d9d1ebfa40559a8248e4d0be2074e
hjohnsick <hjohnsick@yahoo.com>
2022-01-29 12:12:19 -0500

    updated questions array

--- index.js
+++ index.js
@@ -4,14 +4,79 @@ const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your project title?", "Enter a description of your project?", "Enter installation instructions", "Enter usage information",
-"Enter contribution guidelines", "Enter test instructions", "Choose a license", "Enter your GitHub username", "Enter your email address"];
+const questions = [ {
+    type: "input",
+    name: "title",
+    message: "What is your project title?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project:"
+},
+{
+    type: "input",
+    name: "instructions",
+    message: "Enter installation instructions"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Enter usage information",
+},
+{
+    type: "input",
+    name: "guidelines",
+    message: "Enter contribution guidelines",
+},
+{
+    type: "input",
+    name: "test",
+    message: "Enter test instructions",
+},
+{
+    type: "checkbox",
+    name: "licesnses",
+    message: "Choose a license",
+    choices: [
+        "MIT",
+        "Mozilla",
+        "Eclipse",
+        "Apache"
+    ]
+},
+{
+    type: "input",
+    name: "name",
+    message: "Enter your GitHub username"
+},
+{
+    type: "input",
+    name: "email",
+    message: "Enter your email address"
+}
+];
+
+// const promptUser = () => {
+//     return inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "title",
+//             message: "What is your project title?",
+//         }
+//     ])
+// }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+    .then((answers) => {
+        console.log(answers.title);
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+    if (!license) {
+        return '';
+    }
+
+    if (license == "MIT") {
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+    } else if (license == "Mozilla") {
+        return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]';
+    } else if (license == "Eclipse") {
+        return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]';
+    } else if (license == "Apache") {
+        return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (!license) {
+        return '';
+    }
+
+    if (license == "MIT") {
+        return 'https://opensource.org/licenses/MIT';
+    } else if (license == "Mozilla") {
+        return 'https://opensource.org/licenses/MPL-2.0';
+    } else if (license == "Eclipse") {
+        return 'https://opensource.org/licenses/EPL-1.0';
+    } else if (license == "Apache") {
+        return 'https://opensource.org/licenses/Apache-2.0';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
